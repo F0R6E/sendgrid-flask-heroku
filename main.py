940d86ebf3c97099debefb384929e361ff89f1b0
Todd Birchard <toddbirchard@gmail.com>
2019-01-21 16:18:52 -0500

    inital commit

--- main.py
+++ main.py
@@ -1,42 +1,31 @@
 import os
-
 import sendgrid
+import os
 from flask import Flask, request
 from sendgrid.helpers.mail import *
+import urllib2 as urllib
 
 SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/", methods=["POST"])
 def mail():
     if request.method == "POST":
         sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
-        from_email = Email(request.form.get("from_email"))
-        to_email = Email(request.form.get("to_email"))
-        subject = request.form.get("subject")
-        content = Content("text/plain", request.form.get("content"))
-        mail = Mail(from_email, subject, to_email, content)
-        response = sg.client.mail.send.post(request_body=mail.get())
-        if response.status_code == 202:
-            return "Email sent successfully!"
-        else:
-            return "Status Code: " + str(response.status_code)
-    else:
-        return """
-        <html>
-           <body>
-              <form method = "POST">
-                 <p>From: <input type = "text" name = "from_email" value="test@example.com" style="width: 500px;" /></p>
-                 <p>To: <input type = "text" name = "to_email" value="test@example.com" style="width: 500px;" /></p>
-                 <p>Subject: <input type = "text" name = "subject" value="Sending with SendGrid is Fun" style="width: 500px;" /></p>
-                 <p>Content: <input type ="text" name = "content" value="and easy to do anywhere, even with Python" style="width: 500px;" /></p>
-                 <p><input type = "submit" value = "send email" /></p>
-              </form>
-           </body>
-        </html>
-        """
+        from_email = Email(os.environ["FROM_EMAIL"])
+        to_email = Email(request.args.get('email'))
+        mail = Mail(from_email, to_email)
+        mail.template_id = os.environ["TEMPLATE_ID"]
+        try:
+            response = sg.client.mail.send.post(request_body=mail.get())
+        except urllib.HTTPError as e:
+            print(e.read())
+            exit()
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
 
 
 if __name__ == "__main__":
